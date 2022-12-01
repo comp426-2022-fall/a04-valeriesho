02d4475683c5d25b46d152532bc9947e6c2eabcb
VS <valeriesho@unc.edu>
2022-11-30 22:58:24 -0500

    added app listening

--- server.js
+++ server.js
@@ -2,12 +2,27 @@
 
 import minimist from "minimist";
 import express from "express";
-import { roll } from "../lib/roll.js";
+import { roll } from "./lib/roll.js";
 
 const args = minimist(process.argv.slice(2));
 const app = express();
 const port = args.port || 5000;
 
-app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
+app.get('/app/', (req, res, next) => {
+    res.send("200 OK").end();
+});
+
+app.get('/app/roll/', (req, res, next) => {
+    res.send(roll(6,2,1)).end();
+});
+
+app.get('/app/roll/', (req, res, next) => {
+    res.send(roll()).end();
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port + ".");
+});
