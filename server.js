e9aa1546a45e103e285c367811c87fd94b272c28
VS <valeriesho@unc.edu>
2022-11-30 22:47:53 -0500

    attempting app.gets

--- server.js
+++ server.js
@@ -3,3 +3,11 @@
 import minimist from "minimist";
 import express from "express";
 import { roll } from "../lib/roll.js";
+
+const args = minimist(process.argv.slice(2));
+const app = express();
+const port = args.port || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
